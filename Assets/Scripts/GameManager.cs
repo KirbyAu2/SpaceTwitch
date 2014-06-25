7327345a16da7f40dcf30c9b921dd63d928d2aff
Chase Khamashta <ckhamash@ucsc.edu>
2014-06-25 13:06:17 -0700

    Created unity project, start of GameManager and Level classes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public LinkedList<Level> levels;
+	public int currentLevel;
+
+	public int score;
+	public int multiplier;
+	public int lives;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
