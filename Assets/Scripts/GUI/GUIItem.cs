b5290d1fc4b8d94d8f6aa999963f7989e0770c38
adao1123 <adao1193@gmail.com>
2014-06-26 20:28:43 -0700

    GUI Manager & GUI Item. Have not tested it yet, but I will be sure to do that by the end of tommorow

---
+++ Assets/Scripts/GUI/GUIItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIItem {
+    public float xpos;
+    public float ypos;
+    public float starttime;
+    public int dur;
+    public string message;
+    public GUIStyle customGuiStyle;
+
+    public GUIItem(float xposition, float yposition, float start, int duration, string curmessage, GUIStyle cusGuiStyle)
+    {
+        xpos = xposition;
+        ypos = yposition;
+        starttime = start;
+        dur = duration;
+        message = curmessage;
+        customGuiStyle = cusGuiStyle;
+    }
+}
---
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GUIManager : MonoBehaviour {
+    private List<GUIItem> _items;
+
+    // Use this for initialization
+    void Start () {
+	    _items = new List<GUIItem>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GUIItem i in _items)
+        {
+            if (Time.time >= i.starttime + i.dur)
+            {
+                removeGUIItem(i);
+            }
+
+        }
+    }
+    
+    public void addGUIItem(GUIItem g) {
+        _items.Add(g);
+        g.starttime = Time.time;
+    }
+
+    public void removeGUIItem(GUIItem g) {
+        _items.Remove(g);
+    }
+
+    public void clearGUIItem() {
+        _items.Clear();
+    }
+
+    void OnGUI(){
+        foreach (GUIItem i in _items){
+            GUI.Label(new Rect(i.xpos, i.ypos, Screen.width, Screen.height), i.message, i.customGuiStyle);
+        }
+
+    }
+
+}
+
