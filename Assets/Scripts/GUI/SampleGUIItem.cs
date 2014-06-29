a3624ea6286fde201eaf29fff1aa2a13141e21b6
adao1123 <adao1193@gmail.com>
2014-06-28 17:07:20 -0700

    pushed up sample gui script

---
+++ Assets/Scripts/GUI/SampleGUIItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleGUIItem : MonoBehaviour {
+    public GUIStyle style;
+    private bool _displayed = false;
+    // Use this for initialization
+    void Start () {
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        if (GUIManager.Instance && !_displayed)
+        {
+            _displayed = true;
+            GUIManager.Instance.addGUIItem(new GUIItem(100, 100, Time.time, 4, "Testing", style));
+        }
+    }
+}
