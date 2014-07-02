e0f54a8ba2be9c3c835c0b684b7e0fd84fea41eb
adao1123 <adao1193@gmail.com>
2014-07-01 19:17:03 -0700

    worked on swirlie

---
+++ Assets/Scripts/Enemy/Spike.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spike : Enemy
+{
+    public float duration = 0.5f;
+    public float starttime;
+    public float currenttime;
+    public int numOfSpikes = 0;
+    public Spike parent;
+    private List<Spike> _spike;
+    public Spike headSpike;
+    public Spike newSpike;
+    public Spike tailSpike;
+
+    public Spike getHead()
+    {
+        Spike current = this;
+        while (current.parent != null)
+        {
+            current = current.parent;
+        }
+        headSpike = current;
+        return current;
+    }
+
+    private void AddSpike()
+    {
+        if (headSpike == null){
+            headSpike = new Spike();
+        }
+        else{
+            headSpike.AddSpike();
+        }
+        numOfSpikes++;
+    }
+
+    private void RemoveSpike()
+    {
+        /*if () {  //to do: if tailSpike==headSpike
+            //remove headSpike
+            //_alive = false     Spike is dead
+        }
+        else {
+            //remove tailSpike
+            //tailSpike = tailSpike.parent
+            // count--; 
+        }*/
+    }
+
+    void Start()
+    {
+        //list = new MyList();
+        //Spike _spike = new List<Spike>();
+
+    }
+
+    void Update()
+    {
+        /*
+        if (numOfSpikes <= 5)
+        {
+            if (Time.time >= starttime + duration)
+            {
+                AddSpike();
+            }
+        }
+        if () { //To do: if swirlie is dead
+            if (){ //To do: if spike is hit
+                RemoveSpike();
+            }
+        }*/
+    }
+
+    public override void spawn(Lane spawnLane)
+    {
+        //_shootTime = Time.time;
+        _currentLane = spawnLane;
+        _alive = true;
+        gameObject.transform.position = _currentLane.Back + (renderer.bounds.size.y / 2) * _currentLane.Normal;
+        transform.rotation = Quaternion.LookRotation(_currentLane.Normal);
+    }
+}
+ /* Questions:
+        How do I say "if swirlie is dead" ?
+        How did you check collision for player projectile and spike?
+        */
---
+++ Assets/Scripts/Enemy/Swirlie.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swirlie : Enemy {
+
+    //Time it takes until next shot
+    private const float SHOOTING_DELAY = 3.0f;
+
+    private float _shootTime;
+    private Vector3 newPosition;
+    void Start()
+    {
+        _score = 100;
+        newPosition = transform.position;
+    }
+
+    void Update() {
+        if (!Alive)
+        {
+            return;
+        }
+        if (Time.time > _shootTime)
+        {
+            _shootTime = Time.time + SHOOTING_DELAY;
+            spawnProjectile();
+        }
+        PositionChanging();
+    }
+
+    void PositionChanging() {
+        
+        Vector3 positionA = _currentLane.Front;//headSpike.gameObject.transform.position; //to do: make spikeHead
+        Vector3 positionB = _currentLane.Back; //tailSpike.gameObject.transform.position; //to do: make spikeTail
+        if (transform.position == positionB)
+        {
+            newPosition = positionA;
+        }
+        else if (transform.position == positionA)
+        {
+            newPosition = positionB;
+        }
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
+    }
+
+
+    public override void spawn(Lane spawnLane)
+    {
+        _shootTime = Time.time;
+        _currentLane = spawnLane;
+        _alive = true;
+        gameObject.transform.position = _currentLane.Back + (renderer.bounds.size.y / 2) * _currentLane.Normal;
+        transform.rotation = Quaternion.LookRotation(_currentLane.Normal);
+    }
+}
