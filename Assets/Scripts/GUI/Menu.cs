0410b381fdbbc5a4a6fc619e9900936ff0559160
adao1123 <adao1193@gmail.com>
2014-07-26 00:49:51 -0700

    Test 2 Joystick Options Slider

--- Assets/Scripts/GUI/Menu.cs
+++ Assets/Scripts/GUI/Menu.cs
@@ -145,20 +145,23 @@ public class Menu : MonoBehaviour {
             else if (GameManager.Instance.enableSeebright == true)
             {
                 //SFX Volume
-                GUI.Label(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2, ScreenUtil.ScreenHeight / 2,
-                    ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(200)), "Effects Volume", style);
+                GUI.SetNextControlName("0");
+                GUI.Button(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2, ScreenUtil.ScreenHeight / 2,
+                    ScreenUtil.getPixelWidth(400), style.fontSize), "Effects Volume", style);
                 GameManager.effectsVolume = GUI.HorizontalSlider(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2,
                     ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(70), ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(50)),
                     GameManager.effectsVolume, 0f, 1.0f);
                 //Music Volume
-                GUI.Label(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2, ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(120),
-                    ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(200)), "Music Volume", style);
+                GUI.SetNextControlName("1");
+                GUI.Button(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2, ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(120),
+                    ScreenUtil.getPixelWidth(400), style.fontSize), "Music Volume", style);
                 GameManager.musicVolume = GUI.HorizontalSlider(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400))/ 2,
                     ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(190), ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(50)),
                     GameManager.musicVolume, 0f, 1.0f);
                 //Sensitivity Control
-                GUI.Label(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2,
-                    ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(240), ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(200)),
+                GUI.SetNextControlName("2");
+                GUI.Button(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2,
+                    ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(240), ScreenUtil.getPixelWidth(400), style.fontSize),
                     "Sensitivity", style);
                 GameManager.mouseSensitivity = GUI.HorizontalSlider(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(400)) / 2,
                     ScreenUtil.ScreenHeight / 2 + ScreenUtil.getPixelHeight(310), ScreenUtil.getPixelWidth(400), ScreenUtil.getPixelHeight(50)),
@@ -167,7 +170,7 @@ public class Menu : MonoBehaviour {
             if (GameManager.Instance != null) {
                 GameManager.Instance.UpdateMusicVolume();
             }
-            
+            GUI.SetNextControlName("3");
             if (GUI.Button(new Rect((ScreenUtil.ScreenWidth - ScreenUtil.getPixelWidth(200)) / 2, 
                 ScreenUtil.ScreenHeight - ScreenUtil.getPixelHeight(150), ScreenUtil.getPixelWidth(200), style.fontSize), "Back", style)) {
                 _displayOptions = false;
@@ -177,7 +180,53 @@ public class Menu : MonoBehaviour {
                         player.UpdateSensitivity();
                     }
                 }
+                            _focusID = ManageFocus(_focusID, 3);
+            if (SBRemote.GetButtonDown(SBRemote.BUTTON_SELECT))
+            {
+                if (_focusID < 0)
+                {
+                    return;
+                }
+                else if (_focusID == 3)
+                {
+                    _displayOptions = false;
+                }
             }
+            if (SBRemote.GetJoystickDelta(SBRemote.JOY_HORIZONTAL) > 0) {
+                if (_focusID == 0 && GameManager.effectsVolume < 1)
+                {
+                    GameManager.effectsVolume += .01f;
+                }
+                else if (_focusID == 1 && GameManager.musicVolume < 1)
+                {
+                    GameManager.musicVolume += .01f;
+                }
+                else if (_focusID == 2 && GameManager.mouseSensitivity < .5f)
+                {
+                    GameManager.mouseSensitivity += .01f;
+                }
+            }
+            if (SBRemote.GetJoystickDelta(SBRemote.JOY_HORIZONTAL) < 0) {
+                if (_focusID == 0 && GameManager.effectsVolume > 0)
+                {
+                    GameManager.effectsVolume -= .01f;
+                }
+                else if (_focusID == 1 && GameManager.musicVolume > 0)
+                {
+                    GameManager.musicVolume -= .01f;
+                }
+                else if (_focusID == 2 && GameManager.mouseSensitivity > .1f)
+                {
+                    GameManager.mouseSensitivity -= .01f;
+                }
+            }
+
+        }
+        else
+        {
+            _focusID = -1;
+        }
+            
         }
         //Credits Screen
         //Displays Credits
